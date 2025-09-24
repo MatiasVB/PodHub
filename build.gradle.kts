9befea490a7d9a006b8a585fa50ad953f77ae3ff
davrapcan <davrapcan@gmail.com>
2025-09-24 11:12:19 +0200

    v0.0 Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.netflix.dgs.codegen") version "6.3.0"
+    id("org.graalvm.buildtools.native") version "0.10.6"
+}
+
+group = "org.Podhub"
+version = "0.0.1-SNAPSHOT"
+description = "Podhub"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
+    maven { url = uri("https://jitpack.io") }
+}
+
+extra["springModulithVersion"] = "1.4.1"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.modulith:spring-modulith-starter-core")
+    implementation("org.springframework.modulith:spring-modulith-starter-mongodb")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.modulith:spring-modulith-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.modulith:spring-modulith-bom:${property("springModulithVersion")}")
+    }
+}
+
+tasks.generateJava {
+    schemaPaths.add("${projectDir}/src/main/resources/graphql-client")
+    packageName = "org.podhub.podhub.codegen"
+    generateClient = true
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/org/podhub/podhub/PodhubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.podhub.podhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

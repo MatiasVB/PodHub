75244fe93fe9255d7dcb74dc0962b6d7d642cf11
davrapcan <davrapcan@gmail.com>
2025-09-24 11:12:19 +0200

    v0.0 Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "Podhub"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://repo.spring.io/milestone") }
+        maven { url = uri("https://repo.spring.io/snapshot") }
+    }
+}
+
+
+
---
+++ src/main/java/org/podhub/podhub/PodhubApplication.java
@@ -0,0 +1,13 @@
+package org.podhub.podhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodhubApplication.class, args);
+    }
+
+}
